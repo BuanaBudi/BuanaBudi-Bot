95232d55e531823ea0d5eaf36e3ea22dff6967e2
BuanaBudi <78005736+BuanaBudi@users.noreply.github.com>
2021-12-22 19:33:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install -y nodejs libwebp ffmpeg wget tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
+npm i jsdom
+
+echo "[*] se han instalado todas las dependencias, ejecute el comando “npm start ” Para iniciar inmediatamente el script"
